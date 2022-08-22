b44473b1b0d7f8447e9eb4ae71bfd959f59a1598
yuxki <yuxki.ryoshi@gmail.com>
2022-08-22 21:03:11 +0900

    Update testing files, to include file when find_name_opt_include option is on.

--- tests/test.sh
+++ tests/test.sh
@@ -9,6 +9,14 @@ if [ "$expect" != "$result" ]; then
   exit 1
 fi
 
+expect_include=`cat tests/sorted-output-include.txt`
+result_include=`$awk -f sort-nginx-directives.awk -v include_by_find=on tests/input-include.conf | sort `
+
+if [ "$expect_include" != "$result_include" ]; then
+  echo "[ERROR]: Unexpected output is found with find_name_opt_include=on option."
+  exit 1
+fi
+
 expect_err="[ERROR] sort-nginx-directives.awk: sort-nginx-directives@0-9c33b361a14a5021586ff16f1b34bcdc84f1b344d88502a943fc1762fb76c1f6 in the configuration will cause unexpected behavior for this program."
 result_err=`$awk -f sort-nginx-directives.awk tests/input-error.conf 2>&1`
 
