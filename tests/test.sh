d201d576d2447c248cebcb2f1720e85bf58de47e
yuxki <yuxki.ryoshi@gmail.com>
2022-08-23 22:34:39 +0900

    Update testing script, to change from using find_name_opt_include option to find_path_opt_include. And modify sort option.

--- tests/test.sh
+++ tests/test.sh
@@ -2,7 +2,7 @@
 
 awk=$1
 expect=`cat tests/sorted-output.txt`
-result=`$awk -f sort-nginx-directives.awk tests/input.conf | sort `
+result=`$awk -f sort-nginx-directives.awk tests/input.conf | sort -t: -k 1,1n`
 
 if [ "$expect" != "$result" ]; then
   echo "[ERROR]: Unexpected output is found."
@@ -10,10 +10,10 @@ if [ "$expect" != "$result" ]; then
 fi
 
 expect_include=`cat tests/sorted-output-include.txt`
-result_include=`$awk -f sort-nginx-directives.awk -v include_by_find=on tests/input-include.conf | sort `
+result_include=`$awk -f sort-nginx-directives.awk -v "find_path_opt_include=on" tests/input-include.conf | sort -t: -k 1,1n`
 
 if [ "$expect_include" != "$result_include" ]; then
-  echo "[ERROR]: Unexpected output is found with find_name_opt_include=on option."
+  echo "[ERROR]: Unexpected output is found with find_path_opt_include=on option."
   exit 1
 fi
 
